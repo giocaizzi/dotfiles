f15e686879daca1b83f147cb4830e4575d0d18c3
giocaizzi <giocaizzi@gmail.com>
2026-02-20 23:50:32 +0100

    feat: update environment scripts and remove cookiecutter config template

--- .chezmoiscripts/run_env.ps1
+++ .chezmoiscripts/run_env.ps1
@@ -1,2 +1,4 @@
-[Environment]::SetEnvironmentVariable("COOKIECUTTER_CONFIG", ".cookiecutter-config.yaml", "User")
+# This script is intended to be run by chezmoi to set environment variables for the user.
+# You can uncomment the line below and replace "MY_ENV_VAR" and "VALUE" with the desired environment variable name and value.
+# [Environment]::SetEnvironmentVariable("MY_ENV_VAR", "VALUE", [EnvironmentVariableTarget]::User)
 
--- .chezmoiscripts/run_env.sh
+++ .chezmoiscripts/run_env.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-export COOKIECUTTER_CONFIG="~/.cookiecutter-config.yaml"
+# This script is intended to be run by chezmoi to set environment variables for the user.
+# You can uncomment the line below and replace "MY_ENV_VAR" and "VALUE" with the desired environment variable name and value.
+# export MY_ENV_VAR="VALUE"
