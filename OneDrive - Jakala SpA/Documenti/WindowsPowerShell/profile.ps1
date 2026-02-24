243af07ee8790b334c7fa5ddcf2bb3c11b1a7160
giocaizzi <giocaizzi@gmail.com>
2026-02-24 02:18:12 +0100

    profile.ps1: Remove conda init block from PowerShell profile

--- OneDrive - Jakala SpA/Documenti/WindowsPowerShell/profile.ps1
+++ OneDrive - Jakala SpA/Documenti/WindowsPowerShell/profile.ps1
@@ -1,10 +1,3 @@
 # -----------------------
-# conda
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Users\gcaizzi\AppData\Local\miniconda3\Scripts\conda.exe") {
-    (& "C:\Users\gcaizzi\AppData\Local\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
-
-# -----------------------
 # oh-my-posh settings
 oh-my-posh init pwsh --config "~\.mytheme.omp.json" | Invoke-Expression
