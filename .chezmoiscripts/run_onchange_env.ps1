677d78d97f05ae943a3d04183ac1fd0b9e7fd005
giocaizzi <giocaizzi@gmail.com>
2026-04-06 22:08:09 +0200

    chore: remove obsolete run_onchange_env scripts
    Replaced by the untracked ~/.secrets system managed via run_once_init-untracked-env.sh

--- .chezmoiscripts/run_onchange_env.ps1
+++
@@ -1,4 +0,0 @@
-# This script is intended to be run by chezmoi to set environment variables for the user.
-# You can uncomment the line below and replace "MY_ENV_VAR" and "VALUE" with the desired environment variable name and value.
-# [Environment]::SetEnvironmentVariable("MY_ENV_VAR", "VALUE", [EnvironmentVariableTarget]::User)
-
